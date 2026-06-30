b8bdba143db542dc9767a0288dd72ec9ed1c8210
Isako <isako.workmail@gmail.com>
2026-07-01 02:44:52 +0300

    Fix prefabs, Fix import settings, Add TableView, Fix DebugWaiterView

--- Assets/_Project/Scripts/Runtime/Diagnostics/DebugWaiterView.cs
+++ Assets/_Project/Scripts/Runtime/Diagnostics/DebugWaiterView.cs
@@ -6,93 +6,123 @@ using UnityEngine;
 namespace Game.Diagnostics
 {
     /// <summary>
-    /// Shows simple waiter debug state and requested meal visuals.
+    /// Shows the waiter's sticky note and requested meal debug visuals.
     /// </summary>
     public class DebugWaiterView : MonoBehaviour
     {
         [SerializeField] private Waiter _waiter;
-        [SerializeField] private TextMeshPro _text;
-        [SerializeField] private SpriteRenderer _mealSpriteRenderer;
+        [SerializeField] private GameObject _orderNote;
+        [SerializeField] private TextMeshPro _tableNumberLabel;
+        [SerializeField] private SpriteRenderer _orderSpriteRenderer;
 
-        private void Awake()
+        private void OnEnable()
         {
-            if (_waiter != null)
+            _waiter.OnCustomerAssigned += HandleCustomerAssigned;
+            _waiter.OnCustomerUnassigned += HandleCustomerUnassigned;
+            _waiter.OnCustomerWasAsked += HandleCustomerWasAsked;
+            _waiter.OnMealPointEntered += HandleMealPointEntered;
+            _waiter.OnMealPointExited += HandleMealPointExited;
+
+            RefreshVisuals();
+        }
+
+        private void OnDisable()
+        {
+            _waiter.OnCustomerAssigned -= HandleCustomerAssigned;
+            _waiter.OnCustomerUnassigned -= HandleCustomerUnassigned;
+            _waiter.OnCustomerWasAsked -= HandleCustomerWasAsked;
+            _waiter.OnMealPointEntered -= HandleMealPointEntered;
+            _waiter.OnMealPointExited -= HandleMealPointExited;
+        }
+
+        private void HandleCustomerAssigned(Customer _)
+        {
+            HideOrderNote();
+            HideOrderSprite();
+        }
+
+        private void HandleCustomerUnassigned()
+        {
+            HideOrderNote();
+            HideOrderSprite();
+        }
+
+        private void HandleCustomerWasAsked(Customer customer)
+        {
+            ShowOrderNote(customer);
+            HideOrderSprite();
+        }
+
+        private void HandleMealPointEntered()
+        {
+            if (!ShouldShowOrderSprite())
             {
-                _waiter.OnMealPointEntered += OnMealPointEntered;
-                _waiter.OnMealPointExited += OnMealPointExited;
-                _waiter.OnCustomerWasAsked += OnCustomerWasAsked;
-                _waiter.OnCustomerUnassigned += OnCustomerUnassigned;
-            }
-
-            Refresh();
-            HideMealSprite();
-        }
-
-        private void OnCustomerUnassigned()
-        {
-            _text.text = string.Empty;
-        }
-
-        private void OnCustomerWasAsked(Customer customer)
-        {
-            _text.text = customer.Table.TableNumber.ToString();
-        }
-
-        private void OnDestroy()
-        {
-            if (_waiter != null)
-            {
-                _waiter.OnMealPointEntered -= OnMealPointEntered;
-                _waiter.OnMealPointExited -= OnMealPointExited;
-                _waiter.OnCustomerWasAsked -= OnCustomerWasAsked;
-                _waiter.OnCustomerUnassigned -= OnCustomerUnassigned;
-            }
-        }
-
-
-        private void Refresh()
-        {
-            if (_text == null)
-                return;
-
-            var table = _waiter != null && _waiter.IsAssigned ? _waiter.AssignedCustomer.Table : null;
-            _text.text = table != null ? table.TableNumber.ToString() : string.Empty;
-        }
-
-        private void OnMealPointEntered()
-        {
-            if (_mealSpriteRenderer == null)
-                return;
-
-            if (_waiter == null || !_waiter.IsAssigned || _waiter.ServiceState != WaiterServiceState.AwaitingMeal)
-            {
-                HideMealSprite();
+                HideOrderSprite();
                 return;
             }
 
-            if (_waiter.AssignedCustomer.Order != null &&
-                _waiter.AssignedCustomer.Order.TryGetProperty<FoodProperty>(out var spriteProperty))
+            ShowOrderSprite(_waiter.AssignedCustomer);
+        }
+
+        private void HandleMealPointExited()
+        {
+            HideOrderSprite();
+        }
+
+        private void RefreshVisuals()
+        {
+            if (ShouldShowOrderNote())
+                ShowOrderNote(_waiter.AssignedCustomer);
+            else
+                HideOrderNote();
+
+            if (ShouldShowOrderSprite())
+                ShowOrderSprite(_waiter.AssignedCustomer);
+            else
+                HideOrderSprite();
+        }
+
+        private bool ShouldShowOrderNote()
+            => _waiter.IsAssigned &&
+               _waiter.AssignedCustomer != null &&
+               _waiter.ServiceState != WaiterServiceState.Unassigned &&
+               _waiter.ServiceState != WaiterServiceState.AskingCustomer;
+
+        private bool ShouldShowOrderSprite()
+            => ShouldShowOrderNote() &&
+               _waiter.ServiceState == WaiterServiceState.AwaitingMeal &&
+               _waiter.AtMealPoint;
+
+        private void ShowOrderNote(Customer customer)
+        {
+            _orderNote.SetActive(true);
+            _tableNumberLabel.text = customer.Table.TableNumber.ToString();
+        }
+
+        private void HideOrderNote()
+        {
+            _orderNote.SetActive(false);
+            _tableNumberLabel.text = string.Empty;
+        }
+
+        private void ShowOrderSprite(Customer customer)
+        {
+            if (customer.Order != null &&
+                customer.Order.TryGetProperty<FoodProperty>(out var foodProperty) &&
+                foodProperty.DialogueSprite != null)
             {
-                _mealSpriteRenderer.sprite = spriteProperty.WorldSprite;
-                _mealSpriteRenderer.enabled = spriteProperty.WorldSprite != null;
+                _orderSpriteRenderer.sprite = foodProperty.DialogueSprite;
+                _orderSpriteRenderer.enabled = true;
                 return;
             }
 
-            HideMealSprite();
+            HideOrderSprite();
         }
 
-        private void OnMealPointExited()
+        private void HideOrderSprite()
         {
-            HideMealSprite();
-        }
-
-        private void HideMealSprite()
-        {
-            if (_mealSpriteRenderer == null)
-                return;
-
-            _mealSpriteRenderer.sprite = null;
-            _mealSpriteRenderer.enabled = false;
+            _orderSpriteRenderer.sprite = null;
+            _orderSpriteRenderer.enabled = false;
         }
     }
 }
