36bc89735150c6c2dc3efb3eda36666d1f644377
Isako <isako.workmail@gmail.com>
2026-07-07 22:05:19 +0300

    Move ShiftRandomItemDefinitionGiver into a separate script

--- Assets/_Project/Scripts/Runtime/Progression/ShiftService.cs
+++ Assets/_Project/Scripts/Runtime/Progression/ShiftService.cs
@@ -19,27 +19,6 @@ namespace Game.Progression
         public int AvailableRecepies;
     }
 
-    public sealed class ShiftRandomItemDefinitionGiver : IRandomItemDefinitionGiver
-    {
-        private readonly ShiftService _shiftService;
-        private readonly List<ItemDefinition> _allowedItems;
-
-        public ShiftRandomItemDefinitionGiver(ShiftService shiftService, List<ItemDefinition> allowedItems)
-        {
-            _shiftService = shiftService;
-            _allowedItems = allowedItems;
-        }
-
-        /// <inheritdoc/>
-        public ItemDefinition GetRandomItemDefinition()
-        {
-            var unlocked = _allowedItems
-                .FindAll(item => item.TryGetProperty<ShiftProperty>(out var shiftProp)
-                && shiftProp.RequiredShift <= _shiftService.ShiftIndex);
-            return unlocked.Count > 0 ? unlocked[UnityEngine.Random.Range(0, unlocked.Count)] : null;
-        }
-    }
-
     /// <summary>
     /// Stores shift definitions and runs the shift game mode.
     /// </summary>
