972264c7c3c7e176fd8b05053f1f6fdb6d8fc8a0
KarFayInn <fayzakhanovk@gmail.com>
2026-07-13 21:25:08 +0300

    Add Purchase buttons

--- Assets/_Project/Scripts/Runtime/UI/SiteShopItemButton.cs
+++ Assets/_Project/Scripts/Runtime/UI/SiteShopItemButton.cs
@@ -3,6 +3,7 @@ using Game.Items;
 using Game.Services;
 using Game.UI;
 using Itemworks.UnityEngine;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
@@ -20,6 +21,11 @@ namespace Game.UI
         [SerializeField] private bool isDefault;
         [SerializeField] private Sprite lockedSprite;
         [SerializeField] private Sprite unlockedSprite;
+
+        [SerializeField] private int price;
+        private Button purchaseButton;
+
+        private int playerBalance;
         private bool isUnlocked;
 
         private void Awake()
@@ -28,20 +34,49 @@ namespace Game.UI
             if (_button != null)
             {
                 _button.onClick.AddListener(OnButtonClick);
+                _button.interactable = isDefault || isUnlocked;
             }
             _buttonImage = GetComponent<Image>();
             if (_buttonImage != null)
             {
-                _buttonImage.sprite = isDefault? unlockedSprite :lockedSprite;
+                _buttonImage.sprite = isDefault || isUnlocked? unlockedSprite :lockedSprite;
+            }
+
+            purchaseButton = transform.Find("PurchaseButton").GetComponent<Button>();
+            var priceText = transform.Find("PurchaseButton/Price").GetComponent<TextMeshProUGUI>();
+
+            if (purchaseButton != null)
+            {
+                purchaseButton.onClick.AddListener(OnPurchaseClick);
+                purchaseButton.gameObject.SetActive(!isDefault);
+                if (priceText != null)
+                    priceText.text = price.ToString();
+            }
+        }
+
+        private void OnPurchaseClick()
+        {
+            //playerBalance = ServiceLocator.Get<IPlayerBalanceService>().GetBalance(); // пример
+            
+            if (playerBalance >= price)
+            {
+                //ServiceLocator.Get<IPlayerBalanceService>().Spend(price);
+                
+                UnlockVisuals();
+                
+                
+                if (purchaseButton != null)
+                    purchaseButton.gameObject.SetActive(false);
             }
         }
 
         private void OnDestroy()
         {
             if (_button != null)
-            {
                 _button.onClick.RemoveListener(OnButtonClick);
-            }
+            
+            if (purchaseButton != null)
+                purchaseButton.onClick.RemoveListener(OnPurchaseClick);
         }
 
         private void OnButtonClick()
@@ -60,6 +95,7 @@ namespace Game.UI
         {
             isUnlocked = true;
             _buttonImage.sprite = unlockedSprite;
+            _button.interactable = true;
         }
         
         private void ToggleVisuals()
