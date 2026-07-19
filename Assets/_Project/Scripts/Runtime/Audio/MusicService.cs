346affbddf99b9ba71cb714cd55b07b105cce750
coilgung <os6844223@gmail.com>
2026-07-20 01:28:54 +0300

    for shift track and calm track

--- Assets/_Project/Scripts/Runtime/Audio/MusicService.cs
+++ Assets/_Project/Scripts/Runtime/Audio/MusicService.cs
@@ -1,5 +1,6 @@
 using Game.Lifecycle;
 using Game.Services;
+using Game.Progression;
 using UnityEngine;
 using System;
 
@@ -8,6 +9,7 @@ namespace Game.Audio
     [RequireComponent(typeof(AudioSource)), ExecuteInEditMode]
     public class MusicService : MonoBehaviour, IBootstrapable
     {
+        private RunSessionService _runSessionService;
         [SerializeField] private AudioClip[] _musList;
         private AudioSource _audioSource;
 
@@ -18,13 +20,14 @@ namespace Game.Audio
 
         private void Start()
         {
+            _runSessionService = ServiceLocator.Get<RunSessionService>();
             _audioSource = gameObject.GetComponent<AudioSource>();
         }
         private void Update()
         {
             if (!_audioSource.isPlaying)
             { 
-                _audioSource.clip = _musList[0];
+                _audioSource.clip = _runSessionService.State != RunSessionState.Running ? _musList[0] : _musList[1];
                 _audioSource.Play();
             }
         }
